1ba717e766d08d7272c6436e3500aa70667031c0
VinisAndrade <viniciusarmani@gmail.com>
2023-05-24 08:46:51 -0300

    Envio

---
+++ Assets/BinaryFile.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Runtime;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+
+public class BinaryFile {
+
+    
+
+    public static void SavePlayer (Player player) {
+        BinaryFormatter bf = new BinaryFormatter ();
+        FileStream stream = new FileStream (Application.persistentDataPath + "/player.sav", FileMode.Create);
+        
+        Debug.Log(Application.persistentDataPath + "/player.sav");
+        PlayerData data = new PlayerData (player);
+        bf.Serialize(stream, data);
+        
+        stream.Close ();
+    }
+
+    public static int[] LoadPlayer() {
+        if (File.Exists (Application.persistentDataPath + "/player.sav")) {
+            BinaryFormatter bf = new BinaryFormatter ();
+            FileStream stream = new FileStream (Application.persistentDataPath + "/player.sav", FileMode.Open);
+
+            PlayerData data = bf.Deserialize(stream) as PlayerData;
+
+            stream.Close ();
+            return data.stats;
+        } else {
+            Debug.Log("O arquivo não existe");
+            return null;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
+
+[Serializable]
+
+public class PlayerData {
+    public int[] stats;
+
+    public PlayerData (Player player) {
+        stats = new int[3];
+        stats[0] = player.life;
+        stats[1] = player.attack;
+        stats[2] = player.defense;
+
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+    public int attack = 20;
+    public int defense = 2;
+    public int life = 500;
+
+    public Text txtAttack, txtDefense, txtLife;
+
+    // Start is called before the first frame update
+    void Start () {
+        BinaryFile.SavePlayer (this);
+        CarregaDados();
+
+    }
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void CarregaDados () {
+        int[] loadStats = BinaryFile.LoadPlayer ();
+
+        life = loadStats[0];
+        attack = loadStats[1];
+        defense = loadStats[2];
+
+    }
+
+    public void UpdateDisplay () {
+        txtAttack.text = attack.ToString ();
+        txtDefense.text = defense.ToString ();
+        txtLife.text = life.ToString ();
+    }
+}
